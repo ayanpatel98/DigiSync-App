a5641a37466b9e8a4b72729ba1f8458043e21f2d
ayanpatel98 <ayanpatel_98@yahoo.com>
2024-05-25 16:48:57 -0700

    addd project zip file

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 // Mongo connection string
-const mongoURI = "mongodb://localhost:27017";
+// const mongoURI = "mongodb://localhost:27017";
+const mongoURI = "mongodb+srv://ayanpatel69:ayanpatel69@digisync-app.kxmydzz.mongodb.net/?retryWrites=true&w=majority&appName=DigiSync-App";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
